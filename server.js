e59a9c7e8b6bc9f374fcb95ad5fbc5f11294e667
Paschmaria <onuorahpaschal@gmail.com>
2020-07-10 01:18:04 +0100

    Initial MongoDB setup done

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
 
-// load env vars
-dotenv.config({ path: './config/config.env' });
+const connectDB = require('./config/db');
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+// load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// mount routes
+app.use('/api/v1/bootcamps', bootcamps);
+
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow.bold)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server and exit process with failure
+    server.close(() => process.exit(1));
+})
